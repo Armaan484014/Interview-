93a063ede336cc4de2ee387db58eaf58afeb6030
Armaan484014 <140814066+Armaan484014@users.noreply.github.com>
2025-04-12 14:50:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminDashboard from './pages/AdminDashboard';
+import CreateForm from './pages/CreateForm';
+import ManageTemplates from './pages/ManageTemplates';
 import StudentForm from './pages/StudentForm';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<AdminDashboard />} />
-        <Route path='/form/:id' element={<StudentForm />} />
+        <Route path="/" element={<AdminDashboard />} />
+        <Route path="/create-form" element={<CreateForm />} />
+        <Route path="/manage-templates" element={<ManageTemplates />} />
+        <Route path="/form/:id" element={<StudentForm />} /> {/* Student feedback form route */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
