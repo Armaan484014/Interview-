702b18f76d16c547c5747994f11950f08c82eac8
Armaan484014 <140814066+Armaan484014@users.noreply.github.com>
2025-04-12 11:45:36 +0530

    Add files via upload

---
+++ src/pages/AdminDashboard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AdminDashboard = () => {
+  return <div>Admin Dashboard - Form Management</div>;
+};
+
+export default AdminDashboard;
---
+++ src/pages/StudentForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StudentForm = () => {
+  return <div>Student Feedback Form</div>;
+};
+
+export default StudentForm;
