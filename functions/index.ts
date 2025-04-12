4d7fa206139b52e62d30a539a3b933be985a9ce6
Armaan484014 <140814066+Armaan484014@users.noreply.github.com>
2025-04-12 11:32:20 +0530

    Delete functions/index.ts

--- functions/index.ts
+++
@@ -1,7 +0,0 @@
-import * as functions from "firebase-functions";
-import { generateCertificate } from "./generateCertificate";
-import { sendOtp, verifyOtp } from "./otp";
-
-export const createCertificate = functions.https.onCall(generateCertificate);
-export const requestOtp = functions.https.onCall(sendOtp);
-export const checkOtp = functions.https.onCall(verifyOtp);
